6ab9566e54ce0ad36c52e337411e40e23ea33c64
Mery Chavez Pacifico <Mery Chavez Pacifico@DESKTOP-EE0H2A3>
2020-09-07 14:52:30 -0600

    Diseño Formulario Proveedores

---
+++ BusinessControl/FrmProveedores.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace BusinessControl
+{
+    partial class FrmProveedores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuSuperior = new System.Windows.Forms.ToolStrip();
+            this.BtnCerrar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtPrimerNombre = new System.Windows.Forms.TextBox();
+            this.lblNIF = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDui = new System.Windows.Forms.MaskedTextBox();
+            this.txtTelefono1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.MenuSuperior.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuSuperior
+            // 
+            this.MenuSuperior.BackColor = System.Drawing.Color.White;
+            this.MenuSuperior.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BtnCerrar,
+            this.toolStripLabel1});
+            this.MenuSuperior.Location = new System.Drawing.Point(0, 0);
+            this.MenuSuperior.Name = "MenuSuperior";
+            this.MenuSuperior.Size = new System.Drawing.Size(690, 25);
+            this.MenuSuperior.TabIndex = 1;
+            this.MenuSuperior.Text = "toolStrip1";
+            // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.BtnCerrar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BtnCerrar.Image = global::BusinessControl.Properties.Resources.close_button;
+            this.BtnCerrar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(23, 22);
+            this.BtnCerrar.Text = "Salir";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(165, 22);
+            this.toolStripLabel1.Text = "Formulario datos proveedores";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.lblNombre.ForeColor = System.Drawing.Color.White;
+            this.lblNombre.Location = new System.Drawing.Point(32, 48);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(127, 16);
+            this.lblNombre.TabIndex = 5;
+            this.lblNombre.Text = "Nombre Proveedor:";
+            // 
+            // txtPrimerNombre
+            // 
+            this.txtPrimerNombre.Location = new System.Drawing.Point(35, 68);
+            this.txtPrimerNombre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPrimerNombre.Name = "txtPrimerNombre";
+            this.txtPrimerNombre.Size = new System.Drawing.Size(169, 21);
+            this.txtPrimerNombre.TabIndex = 6;
+            // 
+            // lblNIF
+            // 
+            this.lblNIF.AutoSize = true;
+            this.lblNIF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.lblNIF.ForeColor = System.Drawing.Color.White;
+            this.lblNIF.Location = new System.Drawing.Point(280, 47);
+            this.lblNIF.Name = "lblNIF";
+            this.lblNIF.Size = new System.Drawing.Size(32, 16);
+            this.lblNIF.TabIndex = 7;
+            this.lblNIF.Text = "NIF:";
+            this.lblNIF.Click += new System.EventHandler(this.lblNIF_Click);
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.lblCorreo.ForeColor = System.Drawing.Color.White;
+            this.lblCorreo.Location = new System.Drawing.Point(486, 47);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(52, 16);
+            this.lblCorreo.TabIndex = 9;
+            this.lblCorreo.Text = "Correo:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(489, 67);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 21);
+            this.textBox1.TabIndex = 10;
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.lblDireccion.ForeColor = System.Drawing.Color.White;
+            this.lblDireccion.Location = new System.Drawing.Point(32, 117);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(68, 16);
+            this.lblDireccion.TabIndex = 11;
+            this.lblDireccion.Text = "Direccion:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(35, 137);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(275, 21);
+            this.textBox2.TabIndex = 12;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.lblEstado.ForeColor = System.Drawing.Color.White;
+            this.lblEstado.Location = new System.Drawing.Point(32, 190);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(54, 16);
+            this.lblEstado.TabIndex = 13;
+            this.lblEstado.Text = "Estado:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(35, 209);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(282, 21);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(393, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Telefono:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtDui
+            // 
+            this.txtDui.Location = new System.Drawing.Point(283, 67);
+            this.txtDui.Mask = "00000000-0";
+            this.txtDui.Name = "txtDui";
+            this.txtDui.Size = new System.Drawing.Size(144, 21);
+            this.txtDui.TabIndex = 17;
+            // 
+            // txtTelefono1
+            // 
+            this.txtTelefono1.Location = new System.Drawing.Point(396, 137);
+            this.txtTelefono1.Mask = "0000-0000";
+            this.txtTelefono1.Name = "txtTelefono1";
+            this.txtTelefono1.Size = new System.Drawing.Size(74, 21);
+            this.txtTelefono1.TabIndex = 18;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(502, 137);
+            this.maskedTextBox1.Mask = "0000-0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(74, 21);
+            this.maskedTextBox1.TabIndex = 19;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.lblDescripcion.ForeColor = System.Drawing.Color.White;
+            this.lblDescripcion.Location = new System.Drawing.Point(373, 190);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(83, 16);
+            this.lblDescripcion.TabIndex = 20;
+            this.lblDescripcion.Text = "Descripcion:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(376, 209);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(250, 21);
+            this.txtDescripcion.TabIndex = 21;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(452, 246);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(99, 41);
+            this.btnAgregar.TabIndex = 27;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(569, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 41);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "Actualizar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FrmProveedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(690, 299);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.txtTelefono1);
+            this.Controls.Add(this.txtDui);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.lblNIF);
+            this.Controls.Add(this.txtPrimerNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.MenuSuperior);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmProveedores";
+            this.Text = "FrmProveedores";
+            this.MenuSuperior.ResumeLayout(false);
+            this.MenuSuperior.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip MenuSuperior;
+        private System.Windows.Forms.ToolStripButton BtnCerrar;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.Label lblNombre;
+        public System.Windows.Forms.TextBox txtPrimerNombre;
+        private System.Windows.Forms.Label lblNIF;
+        private System.Windows.Forms.Label lblCorreo;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblDireccion;
+        public System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblEstado;
+        public System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.MaskedTextBox txtDui;
+        public System.Windows.Forms.MaskedTextBox txtTelefono1;
+        public System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button button2;
+    }
+}
